a107e0ca7f25b31c604c40fc7ccc29059ac6ac9a
Trepix <pau.trepat.segura@gmail.com>
2024-10-22 20:12:03 +0200

    Remove not anymore necessary named constructor

--- src/main/scala/kata/Person.scala
+++ src/main/scala/kata/Person.scala
@@ -2,17 +2,4 @@ package kata
 
 import java.util.Date
 
-case class Person(name: String, birthDate: Date) {
-
-  def getName(): String = name
-
-  def getBirthDate(): Date = birthDate
-
-}
-
-object Person {
-  def create(name: String, birthDate: Date) = {
-    val p = Person(name, birthDate)
-    p
-  }
-}
+case class Person(name: String, birthDate: Date)
