5a0280fd772bbaaf503a48bc176c3159a0ca66f3
Trepix <pau.trepat.segura@gmail.com>
2024-10-22 23:33:24 +0200

    Reduce to more concise solution for Closes Criteria

--- src/main/scala/kata/Finder.scala
+++ src/main/scala/kata/Finder.scala
@@ -4,23 +4,23 @@ class Finder(private val persons: List[Person]) {
 
   def find(ft: Criteria): Option[Result] = {
 
-    if (persons.size <= 1) {
-      return None
+    persons match {
+      case Nil | _ :: Nil => None
+      case _ =>
+        implicit val personOrdering: Ordering[Person] = Person.orderingByBirthDate
+        val list = persons.sorted
+        val answer = ft match {
+          case Criteria.Closest =>
+            Result.apply _ tupled list
+              .zip(list.tail)
+              .map { case (p1, p2) => (p1, p2) }
+              .minBy {
+                (p1, p2) => p1.timeBetweenBirthdays(p2)
+              }
+          case Criteria.Furthest => Result(list.head, list.last)
+        }
+        Some(answer)
     }
 
-    implicit val personOrdering: Ordering[Person] = Person.orderingByBirthDate
-    val list = persons.sorted
-    val answer = ft match {
-      case Criteria.Closest => {
-        val x = list.zip(list.tail).map { case (p1, p2) => (p1, p2, p1.timeBetweenBirthdays(p2)) }.minBy {
-          case (_, _, timeBetweenBirthdays) => timeBetweenBirthdays
-        }
-        Result(x._1, x._2)
-      }
-      case Criteria.Furthest => {
-        Result(list.head, list.last)
-      }
-    }
-    Some(answer)
   }
 }
